d57c2aa220a25dc9b33c637abddc1e69c1945177
Aniceto Dinis <anicetodinis.ad@gmail.com>
2024-02-13 08:04:43 +0200

    some updates

--- course-code-resources/2.2-s3-static-website/s3-commands.sh
+++ course-code-resources/2.2-s3-static-website/s3-commands.sh
@@ -5,15 +5,21 @@ aws s3 mb s3://mybucket
 aws s3 ls
 
 # upload a file to the s3 bucket
-aws s3 cp yourfile.ext s3://intro-aws-4524
+aws s3 cp yourfile.ext s3://intro-aws-32732ds
+
+aws s3 cp error.html s3://mywebsite-32sad
+aws s3 cp index.html s3://mywebsite-32sad
+aws s3 cp bucket.png s3://mywebsite-32sad
+
 
 # List contents in the s3 bucket
-aws s3 ls s3://intro-aws-4524
+aws s3 ls s3://intro-aws-32732ds
 
 # delete object
-aws s3 rm s3://intro-aws-4524/yourfile.ext
+aws s3 rm s3://intro-aws-32732ds/bucket.png
 
 # delete all objects in a bucket
-aws s3 rm s3://myfirstbucket-53538hd --recursive
+aws s3 rm s3://myfirstbucket-323362fd --recursive
+aws s3 rm s3://mywebsite-32sad --recursive
 # Delete the bucket
-aws s3 rb --force s3://intro-aws-4524
+aws s3 rb --force s3://mywebsite-32sad
--- course-code-resources/2.3-lambdas/lambdas.sh
+++ course-code-resources/2.3-lambdas/lambdas.sh
@@ -2,5 +2,5 @@
 aws lambda invoke --function-name helloWorldFunction out --log-type Tail
 
 # use base64 utility to decode the logs
-aws lambda invoke --function-name helloWorldFunction out --log-type Tail \
+aws lambda invoke --function-name helloWorldFunction out --log-type Tail \ 
 --query 'LogResult' --output text |  base64 -d
